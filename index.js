c9309f403edf77d18417507d7cec68c0455545d5
sean <sean.mcbroom@outlook.com>
2022-02-02 17:37:56 -0600

    initial commit 🐇

---
+++ index.js
@@ -0,0 +1,35 @@
+const { Client, Intents } = require('discord.js');
+
+const client = new Client({
+    intents: new Intents([
+        Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.GUILD_PRESENCES
+    ])
+});
+
+client.on('ready', () => {
+    const Check = () => {
+        client.guilds.cache.forEach(async guild => {
+            guild.members.fetch({ force: true, withPresences: true })
+                .then((members) => {
+                    members.forEach((member) => {
+                        if (member.presence && member.presence.activities) {
+                            member.presence.activities.forEach(activitie => {
+                                console.log(`${member.user.username} - ${activitie.name}`)
+                                if (activitie.name.toLowerCase() == "destiny 2") {
+                                    member.voice.disconnect()
+                                        .catch(console.log);
+                                }
+                            })
+                        }
+                    });
+                })
+                .catch(console.error);
+        });
+    };
+
+    Check();
+    setInterval(Check, 5 * 60 * 1000);
+});
+
+client.login(require('./settings').Key); // Bot Token
